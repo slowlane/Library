625ac3522cfd33a0dd98c5ca596ce7f8c4c9b6be
PerennialWill <andreas.wickerfalt@gmail.com>
2022-03-14 00:36:03 +0100

    Basic stuff finished

---
+++ script.js
@@ -0,0 +1,85 @@
+let myLibrary = [];
+
+//Book constructor
+function Book(title, author, pages){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+
+    this.info = function() {
+        return this.title + " by " + this.author + ", " + pages + " pages, not read yet"; 
+    }
+}
+
+function addBookToLibrary(book){
+    myLibrary.push(book);
+    render();
+}
+
+
+//Render contents of myLibrary
+function render(){
+    let content = document.getElementById("content");
+    content.innerHTML = "";
+
+    for(let book of myLibrary){
+
+        //Create card
+        let bookCard = document.createElement("div");
+        bookCard.className = "card";
+        
+        //Create contents of card
+
+        let bookHeader = document.createElement("h2");
+        bookHeader.innerText = book.title;
+
+        let title = document.createElement("p");
+        title.innerText = "Author: " + book.author;
+
+        let author = document.createElement("p");
+        author.innerText = "Title: " + book.title;
+
+        let pages = document.createElement("p");
+        pages.innerText = "Pages: " + book.pages;
+
+        //Add contents of card, and add card to the content div
+        bookCard.appendChild(bookHeader);
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+        content.appendChild(bookCard);
+
+    }
+
+}
+
+//Add button/form interactivity
+document.getElementById("add-button").addEventListener("click", e => {
+    document.querySelector(".bg-modal").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click", e => {
+    document.querySelector(".bg-modal").style.display = "none";
+});
+
+document.getElementById("form").addEventListener("submit", e => {
+    e.preventDefault();
+    //Retrieve form items
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const pages = document.getElementById("pages");
+    
+    //Create book and add it to library
+    const book = new Book(title.value, author.value, pages.value);
+    addBookToLibrary(book);
+    
+    //Reset form values 
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+
+    //Close modal
+    document.querySelector(".bg-modal").style.display = "none";
+
+})
