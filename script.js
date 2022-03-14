37739c004fceb86e4938474eea3d4f0ac6cf088f
PerennialWill <andreas.wickerfalt@gmail.com>
2022-03-14 01:37:49 +0100

    Made javascript a bit prettier

--- script.js
+++ script.js
@@ -18,65 +18,11 @@ function addBookToLibrary(book){
     render();
 }
 
-
 //Render contents of myLibrary
 function render(){
     let content = document.getElementById("content");
     content.innerHTML = "";
-
-    for(let book of myLibrary){
-
-        //Create card
-        let bookCard = document.createElement("div");
-        bookCard.className = "card";
-        
-        //Create contents of card
-
-        let bookHeader = document.createElement("h2");
-        bookHeader.innerText = book.title;
-
-        let title = document.createElement("p");
-        title.innerText = "Author: " + book.author;
-
-        let author = document.createElement("p");
-        author.innerText = "Title: " + book.title;
-
-        let pages = document.createElement("p");
-        pages.innerText = "Pages: " + book.pages;
-        
-
-        //Fixing the read toggle
-        let readToggleDiv = document.createElement("div");
-
-        let readToggleLabel = document.createElement("label");
-        readToggleLabel.innerText = "Read";
-        readToggleLabel.setAttribute("for", "read-toggle");
-
-        let readToggle = document.createElement("INPUT");
-        readToggle.setAttribute("type", "checkbox");
-        readToggle.id = "read-toggle";
-
-        readToggle.addEventListener("click", e => {
-            e.target.parentElement.parentElement.classList.toggle("read-book");
-            console.log("Hi!");
-        })
-
-
-        //Add contents of card, and add card to the content div
-        bookCard.appendChild(bookHeader);
-        bookCard.appendChild(title);
-        bookCard.appendChild(author);
-        bookCard.appendChild(pages);
-        bookCard.appendChild(readToggleDiv);
-        
-        readToggleDiv.appendChild(readToggleLabel);
-        readToggleDiv.appendChild(readToggle);
-        // bookCard.appendChild(readToggle);
-
-        content.appendChild(bookCard);
-
-    }
-
+    createCards();
 }
 
 //Add button/form interactivity
@@ -117,3 +63,57 @@ let book2 = new Book("The Hobbit", "Pirate Tolkien", 321);
 myLibrary.push(book1, book2);
 
 render();
+
+function createCards() {
+    for(let book of myLibrary){
+
+        //Create card
+        let bookCard = document.createElement("div");
+        bookCard.className = "card";
+        
+        //Create contents of card
+
+        let bookHeader = document.createElement("h2");
+        bookHeader.innerText = book.title;
+
+        let title = document.createElement("p");
+        title.innerText = "Author: " + book.author;
+
+        let author = document.createElement("p");
+        author.innerText = "Title: " + book.title;
+
+        let pages = document.createElement("p");
+        pages.innerText = "Pages: " + book.pages;
+        
+
+        //Fixing the read toggle
+        let readToggleDiv = document.createElement("div");
+
+        let readToggleLabel = document.createElement("label");
+        readToggleLabel.innerText = "Read";
+        readToggleLabel.setAttribute("for", "read-toggle");
+
+        let readToggle = document.createElement("INPUT");
+        readToggle.setAttribute("type", "checkbox");
+        readToggle.id = "read-toggle";
+
+        readToggle.addEventListener("click", e => {
+            e.target.parentElement.parentElement.classList.toggle("read-book");
+        })
+
+
+        //Add contents of card, and add card to the content div
+        bookCard.appendChild(bookHeader);
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+        bookCard.appendChild(readToggleDiv);
+        
+        readToggleDiv.appendChild(readToggleLabel);
+        readToggleDiv.appendChild(readToggle);
+        // bookCard.appendChild(readToggle);
+
+        content.appendChild(bookCard);
+
+    }
+}
